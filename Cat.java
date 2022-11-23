c87f2d88cac4b23bbebc842e10a9fd4b138c8d69
TrickyLover98 <90567386+TrickyLover98@users.noreply.github.com>
2022-11-23 08:36:05 +0800

    Add files via upload

---
+++ Cat.java
@@ -0,0 +1,14 @@
+public class Cat extends Animal {
+	public void eat(){
+		System.out.print("Cats love to eat fish and ");
+	}
+	
+	public void sleep() { 
+		System.out.println("sleep for 7 hours.");
+	}
+	
+	public void makeSound() {
+		System.out.println("Meow meow");
+	}
+} 
+	
---
+++ Dog.java
@@ -0,0 +1,15 @@
+public class Dog extends Animal {
+	public void eat(){
+	
+	System.out.print("Dog love to eat meat and ");
+	}
+	
+	public void sleep() { 
+		System.out.println("sleep for 9 hours.");
+	}
+	
+	public void makeSound() {
+		System.out.println("Arf arf");
+	}
+} 
+	
